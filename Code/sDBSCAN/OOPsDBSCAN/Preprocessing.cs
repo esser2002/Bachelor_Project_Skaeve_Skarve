148fe12415d3a9a69997a3655ba05bd5e4938253
Esser <jones@itu.dk>
2026-02-11 10:39:52 +0100

    Add preprocessing signature

---
+++ Code/sDBSCAN/OOPsDBSCAN/Preprocessing.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPsDBSCAN;
+
+public static class Preprocessing
+{
+    /// <summary>
+    /// The preprocessing step, as described under 'Algorithm 3' in SDCRP by Xu & Pham
+    /// </summary>
+    ///<param name="X">the set of all vectors</param>
+    ///<param name="Rd">the set of random vectors ri</param>
+    ///<param name="k">k nearest and furthest random vectors</param>
+    ///<param name="m">minPoints ish</param>
+    /// <returns></returns>
+    public static List<Node> Preprocess(List<Node> X, List<Node> Rd, int k, int m)
+    {
+        throw new NotImplementedException();
+    }
+}
