09b83cb0157caa84eafe7b016ef9c465b4a01ba6
Esser <jones@itu.dk>
2026-02-11 14:12:35 +0100

    style in preprocessing

--- Code/sDBSCAN/OOPsDBSCAN/Preprocessing.cs
+++ Code/sDBSCAN/OOPsDBSCAN/Preprocessing.cs
@@ -8,20 +8,20 @@ public static class Preprocessing
     /// The preprocessing step, as described under 'Algorithm 3' in SDCRP by Xu & Pham
     /// </summary>
     ///<param name="X">the set of all vectors</param>
-    ///<param name="Rd">the set of random vectors ri</param>
+    ///<param name="randomVectors">the set of random vectors ri</param>
     ///<param name="k">k nearest and furthest random vectors</param>
     ///<param name="m">minPoints ish</param>
     /// <returns></returns>
-    public static void Preprocess(List<Node> X, List<Node> RandomVectors, int k, int m)
+    public static void Preprocess(List<Node> X, List<Node> randomVectors, int k, int m)
     {
-        foreach (Node node in X)
+        foreach (Node qNode in X)
         {
-            PriorityQueue<Node, Double> nearest = new PriorityQueue<Node, Double>();
-            PriorityQueue<Node, Double> furthest = new PriorityQueue<Node, Double>();
+            PriorityQueue<Node, double> nearest = new ();
+            PriorityQueue<Node, double> furthest = new ();
             
-            foreach (Node randomvector in RandomVectors)
+            foreach (Node randomvector in randomVectors)
             {
-                Double similarity = node.Scalar(randomvector);
+                Double similarity = qNode.Scalar(randomvector);
                 if (nearest.Count < k) // if they are not full
                 {
                     nearest.Enqueue(randomvector, similarity);
@@ -36,7 +36,7 @@ public static class Preprocessing
                         nearest.Enqueue(randomvector, similarity);
                     }
                     
-                    furthest.TryPeek( out _, out Double furthestOtherSimilarity);
+                    furthest.TryPeek( out _, out double furthestOtherSimilarity);
                     if (furthestOtherSimilarity < -similarity)
                     {
                         nearest.Dequeue();
@@ -44,11 +44,17 @@ public static class Preprocessing
                     }
                 }
             }
-            node.Nearest = nearest.UnorderedItems.Select(n => n.Element).ToArray();
-            node.Furthest = furthest.UnorderedItems.Select(n => n.Element).ToArray();
+            qNode.Nearest = nearest.UnorderedItems.Select(n => n.Element).ToArray();
+            qNode.Furthest = furthest.UnorderedItems.Select(n => n.Element).ToArray();
         }
     }
 
+    /// <summary>
+    /// Generated gaussian random vectors
+    /// </summary>
+    /// <param name="D">The number of random vectors</param>
+    /// <param name="d">The amount of dimensions</param>
+    /// <returns></returns>
     public static List<Node> GenerateRandomVectors(int D, int d)
     {
         List<Node> randomVectors = new List<Node>();
