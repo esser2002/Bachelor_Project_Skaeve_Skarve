597ed76760a9810d27401b8940e25cd35e7cb959
Esser <jones@itu.dk>
2026-03-02 13:43:36 +0100

    feat: mutual reachability

--- Code/sDBSCAN/sHDBSCAN/HNode.cs
+++ Code/sDBSCAN/sHDBSCAN/HNode.cs
@@ -7,7 +7,7 @@ namespace sHDBSCAN;
 public class HNode : Node
 {
     private Dictionary<Node, double> mutualReachability;
-    public double CoreDist; 
+    public double CoreDist { get; private set; }
     public HNode(string[] input) : base(input)
     {
     }
@@ -19,39 +19,71 @@ public class HNode : Node
     public void setCoreDist(int k)
     {
         PriorityQueue<Node, double> kclosest = new PriorityQueue<Node, double>(); 
-        addKClosestPoints(kclosest, true , k);
-        addKClosestPoints(kclosest, false , k);
+        addKClosestPoints(kclosest, getVisibleNodes() , k);
 
         CoreDist = kclosest.Dequeue().Dist(this);
     }
 
-    private void addKClosestPoints(PriorityQueue<Node, double> queue, bool closest, int k)
+    private void addKClosestPoints(PriorityQueue<Node, double> queue, IEnumerable<Node> nodes, int k)
     {
-        List<Node> nodes = new List<Node>(closest ? Nearest : Furthest);
-        foreach (Node randomN in nodes)
+        foreach (Node n in nodes)
         {
-            List<Node> othernodes = new List<Node>(closest ? randomN.Nearest : randomN.Furthest);
-            foreach (Node n in othernodes)
+            double dist = Dist(n);
+            if (queue.UnorderedItems.Select(s => s.Element).Contains(n))
             {
-                double dist = Dist(n);
-                if (queue.UnorderedItems.Select(s => s.Element).Contains(n))
+                continue;
+            }
+            if (queue.Count < k)
+            {
+                queue.Enqueue(n,-dist);
+            }
+            else
+            {
+                queue.TryPeek(out _, out double otherDist); //check the distance of the furthest point in queue
+                if (otherDist < -dist)
                 {
-                    continue;
-                }
-                if (queue.Count < k)
-                {
+                    queue.Dequeue();
                     queue.Enqueue(n,-dist);
-                }
-                else
-                {
-                    queue.TryPeek(out _, out double otherDist); //check the distance of the furthest point in queue
-                    if (otherDist < -dist)
-                    {
-                        queue.Dequeue();
-                        queue.Enqueue(n,-dist);
-                    }
                 }
             }
         }
+    }
+
+    private HashSet<HNode> getVisibleNodes()
+    {
+        HashSet<Node> visibleNodes = new();
+        foreach (Node nearRandomNode in Nearest)
+        {
+            foreach (Node nearNearNode in nearRandomNode.Nearest)
+            {
+                visibleNodes.Add(nearNearNode);
+            }
+        }
+        foreach (Node farRandomNode in Furthest)
+        {
+            foreach (Node farFarNode in farRandomNode.Furthest)
+            {
+                visibleNodes.Add(farFarNode);
+            }
+        }
+
+        return visibleNodes.Cast<HNode>().ToHashSet();
+    }
+
+    public void SetMutualReachability()
+    {
+        mutualReachability = new();
+
+        foreach (HNode node in getVisibleNodes())
+        {
+            double dist = new List<double> { CoreDist, node.CoreDist, Dist(node)}.Max();
+            mutualReachability.Add(node, dist);
+        }
+        
+    }
+
+    public double GetReachability(Node node)
+    {
+        return mutualReachability[node];
     }
 }
