55049f401eec05668a464f4bfd0a0a22b8ccc2ad
Marie <mblh@itu.dk>
2026-03-02 12:09:11 +0100

    created HNode and find coredistance hdbscan

---
+++ Code/sDBSCAN/sHDBSCAN/HNode.cs
@@ -0,0 +1,53 @@
+using System.Runtime.InteropServices.Swift;
+using FSharpx.Collections.Tagged;
+using OOPsDBSCAN;
+
+namespace sHDBSCAN;
+
+public class HNode : Node
+{
+    private Dictionary<Node, double> mutualReachability;
+    private double coreDist; 
+    public HNode(string[] input) : base(input)
+    {
+    }
+
+    public HNode(int dimensions) : base(dimensions)
+    {
+    }
+
+    public void setCoreDist(int k)
+    {
+        PriorityQueue<Node, double> kclosest = new PriorityQueue<Node, double>(); 
+        addKClosestPoints(kclosest, true , k);
+        addKClosestPoints(kclosest, false , k);
+
+        coreDist = kclosest.Dequeue().Dist(this);
+    }
+
+    private void addKClosestPoints(PriorityQueue<Node, double> queue, bool closest, int k)
+    {
+        List<Node> nodes = new List<Node>(closest ? Nearest : Furthest);
+        foreach (Node randomN in nodes)
+        {
+            List<Node> othernodes = new List<Node>(closest ? randomN.Nearest : randomN.Furthest);
+            foreach (Node n in othernodes)
+            {
+                double dist = Dist(n);
+                if (queue.Count < k)
+                {
+                    queue.Enqueue(n,-dist);
+                }
+                else
+                {
+                    queue.TryPeek(out _, out double otherDist); //check the distance of the furthest point in queue
+                    if (otherDist < -dist)
+                    {
+                        queue.Dequeue();
+                        queue.Enqueue(n,-dist);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Code/sDBSCAN/sHDBSCAN/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using OOPsDBSCAN;
+using sHDBSCAN;
+
+var path = args[0];
+using TextFieldParser csvParser = new TextFieldParser(path);
+
+csvParser.SetDelimiters(",");
+csvParser.HasFieldsEnclosedInQuotes = true;
+
+// Skip the row with the column names
+csvParser.ReadLine();
+
+List<HNode> dataPoints = [];
+
+while (!csvParser.EndOfData)
+{
+    // Read current line fields, pointer moves to the next line.
+    string[] fields = csvParser.ReadFields() ?? throw new InvalidOperationException();
+    dataPoints.Add(new HNode(fields));
+}
+
+//Normalise data
+foreach (Node node in dataPoints)
+{
+    node.Normalise();
+}
+
+int D = 128; // amount of random vectors 
+int k = 2; //k is the amount off points for creating core distance
+int m = 300; //amount of datapoints each random vector knows
+int l = 2; //amount of random vectors each datapoint knows
+
+List<Node> randomVectors = Preprocessing.GenerateRandomVectors(D, dataPoints[0].Vector.Length);
+
+foreach (Node node in randomVectors)
+{
+    node.Normalise();
+}
+
+Console.WriteLine("Preprocessing");
+Preprocessing.Preprocess(dataPoints.Cast<Node>().ToList(), randomVectors, l, m);
+foreach (HNode n in dataPoints)
+{
+     n.setCoreDist(k);
+}
