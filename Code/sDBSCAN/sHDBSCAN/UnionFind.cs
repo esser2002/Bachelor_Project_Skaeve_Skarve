faac8f89f1dac1bcaac3245938b77be190443a4f
Marie <mblh@itu.dk>
2026-03-09 10:30:48 +0100

    Create UnionFind.cs

---
+++ Code/sDBSCAN/sHDBSCAN/UnionFind.cs
@@ -0,0 +1,68 @@
+namespace sHDBSCAN;
+/// <summary>
+/// inspired by Algorithms fourth edition by Sedgewick and Wayne page 228.
+/// </summary>
+public class UnionFind
+{
+    private int[] id;
+    private int[] size;
+    public int count { get; private set; }//number of components 
+
+    public UnionFind(int N)
+    {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            id[i] = i;
+        }
+        size = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            id[i] = 1;
+        }
+        
+    }
+
+    public bool connected(int p, int q)
+    {
+        return find (p) == find (q); 
+    }
+
+    private int find(int p)
+    {
+        int root = p; 
+        while (root != id[root])
+        {
+            root = id[root];
+        }
+
+        while (root != p) //path compression
+        {
+            int newp = id[p];
+            id[p] = root;
+            p = newp;
+        }
+        return root;
+    }
+
+    public void union(int p, int q)
+    {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
+        
+        if (size[i] < size[j])
+        {
+            id[i] = j;
+            size[j] += size[i];
+        }
+        else
+        {
+            id[j] = i;
+            size[i] += size[j];
+        }
+
+        count--;
+    }
+}
