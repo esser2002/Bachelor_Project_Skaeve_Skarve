f3f52a2d1132f77e7fae7daa9a9a040aed277ff1
kassandra <kwad@itu.dk>
2026-03-18 13:55:40 +0100

    ccc file fixed up (hopefully)

--- Code/sDBSCAN/sHDBSCAN/python/ccc.py
+++ Code/sDBSCAN/sHDBSCAN/python/ccc.py
@@ -1,44 +1,32 @@
-# remember to source ~/hdbscan/.venv/bin/activate to enter environment
 from scipy.cluster.hierarchy import cophenet
 from scipy.stats import pearsonr
 import numpy
-import csv
-import pandas
 import hdbscan
 
+# C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\normalisedDataFashion.csv
+# /Users/mariehansen/Desktop/Bachelor/Bachelor_Project_Skaeve_Skarve/Code/sDBSCAN/data/out/normalisedDataFashion.csv
+normalized_data = numpy.genfromtxt(r"C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\normalisedDataFashion.csv", delimiter=";")
 
-data = numpy.genfromtxt("/Users/mariehansen/Desktop/Bachelor/Bachelor_Project_Skaeve_Skarve/Code/sDBSCAN/data/out/normalisedDataFashion.csv", delimiter=";")
-print (data)      
+# C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\dendrogram.csv
+# /Users/mariehansen/Desktop/Bachelor/Bachelor_Project_Skaeve_Skarve/Code/sDBSCAN/data/out/dendrogram.csv
 
-
-
+approx_dendrogram = numpy.genfromtxt(r"C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\dendrogram.csv", delimiter=";", skip_header=1)
         
-clusterer = hdbscan.HDBSCAN(min_cluster_size=2, gen_min_span_tree=True)
-clusterer.fit(data)
-singleclustertrue = clusterer.single_linkage_tree_.to_numpy()
+clusterer = hdbscan.HDBSCAN(min_cluster_size = 2, gen_min_span_tree=True)
+clusterer.fit(normalized_data)
+true_dendrogram = clusterer.single_linkage_tree_.to_numpy()
 
-dendrogram = []
+print("normalized_data:", normalized_data.shape)
+print("approx_dendrogram:", len(approx_dendrogram))
+print("true_dendrogram:", true_dendrogram.shape)
 
-with open('/Users/mariehansen/Desktop/Bachelor/Bachelor_Project_Skaeve_Skarve/Code/sDBSCAN/data/out/dendrogram.csv') as csvdatafile:
-     # read file as csv file 
-    csvreader = csv.reader(csvdatafile, delimiter=";")
-    next(csvreader)
-
-    #for every row, print the row
-    for row in csvreader:
-        dendrogram.append ([float(row[0]),float(row[1]),float(row[2]),float(row[3])])
-
-
-cd_approx = cophenet(dendrogram)
-cd_true = cophenet(singleclustertrue)
-
+cd_approx = cophenet(approx_dendrogram)
+cd_true = cophenet(true_dendrogram)
 ccc = pearsonr(cd_true, cd_approx)[0]
 
 mr = numpy.exp(numpy.mean(numpy.log(cd_approx / cd_true)))
 
 print("CCC: ", ccc)
-print("MR: ", mr)
- 
- 
- 
+print("MR: ", mr) 
+
 
