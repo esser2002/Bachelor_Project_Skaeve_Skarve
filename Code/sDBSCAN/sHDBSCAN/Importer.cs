f179f0b85ade3e925f4bf960e6a5a9a3db87032f
kassandra <kwad@itu.dk>
2026-03-25 15:01:05 +0100

    feat: cleapup HDBSCAN Program

---
+++ Code/sDBSCAN/sHDBSCAN/Importer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace sHDBSCAN;
+
+public static class Importer
+{
+    public static Dictionary<int, HNode> ImportNodes(string path)
+    {
+        using TextFieldParser csvParser = new TextFieldParser(path);
+
+        csvParser.SetDelimiters(",");
+        csvParser.HasFieldsEnclosedInQuotes = true;
+        csvParser.ReadLine(); // Skip the row with the column names
+        
+        Dictionary<int, HNode> dataPoints = new();
+
+        while (!csvParser.EndOfData)
+        {
+            // Read current line fields, pointer moves to the next line.
+            string[] fields = csvParser.ReadFields() ?? throw new InvalidOperationException();
+            HNode node = new HNode(fields);
+            dataPoints.Add(node.id, node);
+        }
+        return dataPoints;
+    }
+    
+    
+    
+}
--- Code/sDBSCAN/sHDBSCAN/Program.cs
+++ Code/sDBSCAN/sHDBSCAN/Program.cs
@@ -6,89 +6,69 @@ using sHDBSCAN;
 using Exporter = sHDBSCAN.Exporter;
 using Node = Core.Node;
 
-if (args.Length < 2)
-{
-    throw new Exception("Must give 2 arguments (path to data, path to output)")
-        ;}
-
-Stopwatch w = new Stopwatch();
-w.Start();
-
-var path = args[0];
-using TextFieldParser csvParser = new TextFieldParser(path);
-
-csvParser.SetDelimiters(",");
-csvParser.HasFieldsEnclosedInQuotes = true;
-
-// Skip the row with the column names
-csvParser.ReadLine();
-
-List<HNode> dataPoints = [];
-Dictionary<int, HNode> getNode = new();
-
-while (!csvParser.EndOfData)
-{
-    // Read current line fields, pointer moves to the next line.
-    string[] fields = csvParser.ReadFields() ?? throw new InvalidOperationException();
-    HNode node = new HNode(fields);
-    dataPoints.Add(node);
-    getNode.Add(node.id, node);
-}
-
-//Normalise data
-foreach (Node node in dataPoints)
-{
-    node.Normalise();
-}
-
-//Exporter.ExportNormalisedData(args[1], dataPoints);
-
+// --- PARAMETERS --- //
 int k = 4; //k is the amount off points for creating core distance
 int D = 16; //amount of random vectors 
 int l = 2; //amount of random vectors each datapoint knows
 int m = 10; //amount of datapoints each random vector knows
 
-Stopwatch stopWatch = new Stopwatch();
-stopWatch.Start();
+Dictionary<int, HNode> dataPoints = Importer.ImportNodes(args[0]);
 
-List<Node> randomVectors = Preprocessing.GenerateRandomVectors(D, dataPoints[0].Vector.Length);
+if (args.Length < 3)
+{
+    throw new Exception("Must give 3 arguments (path to data, path to output)");
+}
 
-stopWatch.Stop();
-foreach (Node node in randomVectors)
+
+// --- Normalise data --- //
+foreach (HNode node in dataPoints.Values)
 {
     node.Normalise();
 }
 
+Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
+TimeSpan lastTime = stopWatch.Elapsed;
+
+Console.WriteLine("Generating random vectors");
+List<Node> randomVectors = Preprocessing.GenerateRandomVectors(D, dataPoints[0].Vector.Length);
+
+foreach (Node node in randomVectors)
+{
+    node.Normalise();
+}
+printLap("Random vectors");
 
 Console.WriteLine("Preprocessing");
-Preprocessing.Preprocess(dataPoints.Cast<Node>().ToList(), randomVectors, l, m);
+Preprocessing.Preprocess(dataPoints.Values.Cast<Node>().ToList(), randomVectors, l, m);
+printLap("Preprocessing");
 
 Console.WriteLine("Set visible nodes");
-foreach (HNode n in dataPoints)
+foreach (HNode n in dataPoints.Values)
 {
     n.SetVisibleNodes();
 }
+printLap("Set visible nodes");
 
 Console.WriteLine("Set core dist");
-foreach (HNode n in dataPoints)
+foreach (HNode n in dataPoints.Values)
 {
      n.setCoreDist(k);
 }
+printLap("Set Core Dist");
 
 Console.WriteLine("Set mutual reachability");
-foreach (HNode n in dataPoints)
+foreach (HNode n in dataPoints.Values)
 {
     n.SetMutualReachability();
 }
-
+printLap("Set Mutual reachability");
 
 Console.WriteLine("CreateMST");
 var MST = sHDBSCAN.MST.CreateSpanningTree(dataPoints[0]);
 
-//Exporter.ExportMST(args[1],MST);
-
 Console.WriteLine("MST size: " + MST.Count);
+printLap("MST");
 
 Console.WriteLine("Cluster tree");
 UnionFind uf = new UnionFind(dataPoints.Count);
@@ -99,12 +79,12 @@ for (int i = 0; MST.TryDequeue(out Edge edge, out double dist); i++)
 {
     int fromId = edge.From.id;
     int toId = edge.To.id;
-    Console.WriteLine("union " + fromId + ", " + toId + ": " + dist + "(" + edge.From.CoreDist + ", " + edge.To.CoreDist + ", " + edge.From.Dist(edge.To) + ")");
     
     if(uf.Connected(fromId, toId)) {Console.WriteLine("Something wrong, edges are already connected");}
     var union = uf.Union(edge.From.id, edge.To.id);
     dendrogram[i] = (union[0], union[1], dist,union[2]);
 }
+printLap("Cluster tree");
 
 stopWatch.Stop();
 Exporter.ExportDendrogram(args[1],dendrogram);
@@ -115,10 +95,9 @@ string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.M
 
 Exporter.ExportsHdbscanStats(args[2], D, k, l, m, elapsedTime );
 
-w.Stop();
 
 Console.WriteLine($"SkarvHDBSCAN with  k {k}, D {D}, l {l}, m {m}");
-Console.WriteLine("Total runtime:" + w.Elapsed);
+Console.WriteLine("Total time " + elapsedTime);
 
 /*
 //Print the resulting clusters
@@ -148,3 +127,11 @@ foreach (int[] cluster in clusters)
 Console.WriteLine(uf.Count);
 Console.WriteLine("done");
 */
+
+
+void printLap(string lapName)
+{
+    var lap = stopWatch.Elapsed - lastTime;
+    Console.WriteLine(lapName + " done: " + String.Format("{0:00}:{1:00}:{2:00}.{3:00}", lap.Hours, lap.Minutes, lap.Seconds, lap.Milliseconds));
+    lastTime = stopWatch.Elapsed;
+}
