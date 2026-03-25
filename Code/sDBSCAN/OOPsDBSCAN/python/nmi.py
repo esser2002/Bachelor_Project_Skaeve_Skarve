0d4af680e172d717ddf3ec24c58f1ead1a30ed57
kassandra <kwad@itu.dk>
2026-03-25 13:19:28 +0100

    fix:  nmi correctly calculates B)

--- Code/sDBSCAN/OOPsDBSCAN/python/nmi.py
+++ Code/sDBSCAN/OOPsDBSCAN/python/nmi.py
@@ -2,12 +2,28 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics.cluster import normalized_mutual_info_score 
 
-our_clusters = np.genfromtxt(r"C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\sDBSCANclusters.csv",dtype=int)
+sDBSCAN = np.genfromtxt(r"C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\sDBSCANclusters.csv",dtype=int)
+normalized_data = np.genfromtxt(r"C:\Users\kassa\Desktop\bachelor\Bachelor_Project_Skaeve_Skarve\Code\sDBSCAN\data\out\normalisedDataFashion.csv", delimiter=";", skip_header=1)
+clustering = DBSCAN(eps=0.18, min_samples=10, metric='cosine').fit(normalized_data)
 
-X = np.array([[1, 2], [2, 2], [2, 3],
-              [8, 7], [8, 8], [25, 80]])
-clustering = DBSCAN(eps=3, min_samples=2).fit(X)
+nmi = normalized_mutual_info_score(sDBSCAN, clustering.labels_)
 
-nmi = normalized_mutual_info_score(our_clusters, clustering.labels_)
 
-print(nmi)
+cl = {}
+print("DBSCAN clusters:")
+for elem in clustering.labels_:
+    if elem not in cl:
+        cl[int(elem)] = 1
+    else:
+        cl[int(elem)] = cl[int(elem)] + 1
+print(cl)
+cl = {}
+print("sDBSCAN clusters")
+for elem in sDBSCAN:
+    if elem not in cl:
+        cl[int(elem)] = 1
+    else:
+        cl[int(elem)] = cl[int(elem)] + 1
+
+print(cl)
+print(f"nmi : {(100 * nmi):.2f}%")
