6317d4096a15ad101a9e8c7db73101e97397d560
kassandra <kwad@itu.dk>
2026-04-20 14:37:35 +0200

    Comments to nmi.py

--- Code/BA_Scalable_Clustering/sDBSCAN/python/nmi.py
+++ Code/BA_Scalable_Clustering/sDBSCAN/python/nmi.py
@@ -2,15 +2,18 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn.metrics.cluster import normalized_mutual_info_score 
 
-pathToOut = r"C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skæve_Skarve\Code\sDBSCAN\data\out" #Set this to your personal path
-
+# input your local path to data/out
+pathToOut = r"C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skæve_Skarve\Code\sDBSCAN\data\out" 
+#this reads our sDBSCAN output - remember to run sDBSCAN before running this file.
 sDBSCAN = np.genfromtxt(pathToOut+r"\sDBSCANclusters.csv",dtype=int)
+# Input the path to a normalized data csv file. Remember to run the NormaliseData Program beforehand.
 normalized_data = np.genfromtxt(pathToOut+r"\normalisedDataFashion.csv", delimiter=";", skip_header=1)
+# Perform sklearns DBSCAN
 clustering = DBSCAN(eps=0.08, min_samples=50, metric='cosine').fit(normalized_data)
-
+# Perform nmi
 nmi = normalized_mutual_info_score(sDBSCAN, clustering.labels_)
 
-
+# Print results
 cl = {}
 print("DBSCAN clusters:")
 for elem in clustering.labels_:
