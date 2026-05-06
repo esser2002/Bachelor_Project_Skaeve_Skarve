3d831d833b5e281ef7ef52bb175affb13f2fbe38
Esser <jones@itu.dk>
2026-05-06 14:37:21 +0200

    fix: setup easier experiments

--- Code/BA_Scalable_Clustering/sHDBSCAN/RunExperiment.bat
+++ Code/BA_Scalable_Clustering/sHDBSCAN/RunExperiment.bat
@@ -1,3 +1,9 @@
-dotnet run "C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\fashion-mnist_test.csv" "C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\out\dendrogram.csv" "C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\out\sHDBSCANStats.csv"
+set data="C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\mnist_train_2000.csv"
+
+cd ..\NormaliseData\
+dotnet run %data% "..\data\out\normalisedData.csv"
+cd ..\sHDBSCAN\
+dotnet run %data% "C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\out\dendrogram.csv" "C:\Users\jonas\OneDrive\Dokumenter\ITU\Bachelor_Project_Skaeve_Skarve\Code\BA_Scalable_Clustering\data\out\sHDBSCANStats.csv"
 cd .\python\
 python .\ccc.py
+cd ..
